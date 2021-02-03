153da78df50a717731274d500367044054ec91c5
ABHISHEK SUDHIR <abhisheksudhir2000@gmail.com>
2021-02-03 19:09:47 +0530

    connected to mongo atlas

--- blog server/index.js
+++ blog server/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config(); //to create private variables
 const port = process.env.PORT || 5000;
 const app = express();
+
+const uri = process.env.ATLAS_URI; //uri is supposed to be got from mongo atlas
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDb connected");
+});
 
 app.route("/").get((req, res) => res.json("your first rest api"));
 
