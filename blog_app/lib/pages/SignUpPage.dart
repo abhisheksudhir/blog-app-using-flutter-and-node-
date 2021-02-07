1cf54d29ece766c18c6b00bfa13d5ed588e40f2d
ABHISHEK SUDHIR <abhisheksudhir2000@gmail.com>
2021-02-07 15:19:36 +0530

    form validation and using listener to close keyboard when clicked outside of text area

--- blog_app/lib/pages/SignUpPage.dart
+++ blog_app/lib/pages/SignUpPage.dart
@@ -8,62 +8,97 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  bool vis = true;
+  final _globalkey = GlobalKey<FormState>();
+  // final _usernameFocusNode = FocusNode();
+  // final _emailFocusNode = FocusNode();
+  // final _passwordFocusNode = FocusNode();
+
+  // @override
+  // void dispose() {
+  //   // _usernameFocusNode.dispose();
+  //   // _emailFocusNode.dispose();
+  //   // _passwordFocusNode.dispose();
+  //   // TODO: implement dispose
+  //   super.dispose();
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          alignment: Alignment.center,
-          // height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.white, Colors.green[200]],
-              begin: const FractionalOffset(0.0, 1.0),
-              end: const FractionalOffset(0.0, 1.0),
-              stops: [0.0, 1.0],
-              tileMode: TileMode.repeated,
+    return Listener(
+      onPointerDown: (_) {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus &&
+            currentFocus.focusedChild != null) {
+          currentFocus.focusedChild.unfocus();
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: Container(
+            alignment: Alignment.center,
+            // height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.white, Colors.green[200]],
+                begin: const FractionalOffset(0.0, 1.0),
+                end: const FractionalOffset(0.0, 1.0),
+                stops: [0.0, 1.0],
+                tileMode: TileMode.repeated,
+              ),
             ),
-          ),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Sign up with email",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2,
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                usernameTextField(),
-                emailTextField(),
-                passwordTextField(),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  width: 150,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: Color(0xff00A86B),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Sign Up",
+            child: SingleChildScrollView(
+              child: Form(
+                key: _globalkey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Sign up with email",
                       style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
+                        fontSize: 30,
                         fontWeight: FontWeight.bold,
+                        letterSpacing: 2,
                       ),
                     ),
-                  ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    usernameTextField(),
+                    emailTextField(),
+                    passwordTextField(),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        if (_globalkey.currentState.validate()) {
+                          // we send data to rest api server
+                          print("Validated");
+                        }
+                      },
+                      child: Container(
+                        width: 150,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Color(0xff00A86B),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
@@ -78,6 +113,15 @@ class _SignUpPageState extends State<SignUpPage> {
         children: [
           Text("Username"),
           TextFormField(
+            // focusNode: _usernameFocusNode,
+            textInputAction: TextInputAction.next,
+            // onFieldSubmitted: (_) {
+            //   FocusScope.of(context).requestFocus(_emailFocusNode);
+            // },
+            validator: (value) {
+              if (value.isEmpty) return "Username can't be empty";
+              return null;
+            },
             decoration: InputDecoration(
               focusedBorder: UnderlineInputBorder(
                 borderSide: BorderSide(
@@ -99,6 +143,17 @@ class _SignUpPageState extends State<SignUpPage> {
         children: [
           Text("Email"),
           TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            // focusNode: _emailFocusNode,
+            textInputAction: TextInputAction.next,
+            // onFieldSubmitted: (_) {
+            //   FocusScope.of(context).requestFocus(_passwordFocusNode);
+            // },
+            validator: (value) {
+              if (value.isEmpty) return "Email can't be empty";
+              if (!value.contains("@")) return "Email is Invalid";
+              return null;
+            },
             decoration: InputDecoration(
               focusedBorder: UnderlineInputBorder(
                 borderSide: BorderSide(
@@ -120,7 +175,22 @@ class _SignUpPageState extends State<SignUpPage> {
         children: [
           Text("Password"),
           TextFormField(
+            // focusNode: _passwordFocusNode,
+            validator: (value) {
+              if (value.isEmpty) return "Password can't be empty";
+              if (value.length < 8) return "Password lenght must have >=8";
+              return null;
+            },
+            obscureText: vis,
             decoration: InputDecoration(
+              suffixIcon: IconButton(
+                icon: Icon(vis ? Icons.visibility_off : Icons.visibility),
+                onPressed: () {
+                  setState(() {
+                    vis = !vis;
+                  });
+                },
+              ),
               helperText: "Password length should have >=8",
               helperStyle: TextStyle(
                 fontSize: 14,
